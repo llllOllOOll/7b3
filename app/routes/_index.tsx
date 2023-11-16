05e4282c9f2ea030b904b0f1caf9d48e22da3881
llllOllOOll <7b37b3@gmail.com>
2023-11-16 07:34:23 -0300

    users route added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,3 @@
-import { json } from '@remix-run/node'
-import { sql } from '@vercel/postgres'
-
-export async function loader() {
-	const { rows } = await sql`SELECT first_name FROM employees`
-	console.log('Rows', rows)
-	return json(rows)
+export default function Home() {
+	return <h1>Seven</h1>
 }
---
+++ app/routes/users.tsx
@@ -0,0 +1,8 @@
+import { json } from '@remix-run/node'
+import { sql } from '@vercel/postgres'
+
+export async function loader() {
+	const { rows } = await sql`SELECT first_name FROM employees`
+	console.log('Rows', rows)
+	return json(rows)
+}
