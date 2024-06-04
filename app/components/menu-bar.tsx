f7e653687020227085e48b28cc074dec1e14bf1a
seven <7b37b3@gmail.com>
2024-06-04 13:49:29 -0300

    chore: Extracted menu bar to a component

---
+++ app/components/menu-bar.tsx
@@ -0,0 +1,24 @@
+import { Button } from "~/components/ui/button"
+
+const MenuBar = () => {
+	return (
+		<header className="container flex items-center justify-between  px-28 py-8">
+			<div className="flex items-center gap-4">
+				<img src="/img/logo.svg" alt="" />
+				<nav>
+					<ul className="flex gap-4">
+						<li>Features</li>
+						<li>Princing</li>
+						<li>Resources</li>
+					</ul>
+				</nav>
+			</div>
+			<div>
+				<span className="mr-4">Login</span>
+				<Button>Sign Up</Button>
+			</div>
+		</header>
+	)
+}
+
+export { MenuBar }
