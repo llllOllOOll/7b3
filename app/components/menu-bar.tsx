5169c9994009e46620442a4eb8fdfcb959e8b70d
seven <7b37b3@gmail.com>
2024-06-04 15:22:53 -0300

    feat: Responsive menu

--- app/components/menu-bar.tsx
+++ app/components/menu-bar.tsx
@@ -1,21 +1,31 @@
+import { Link } from "@remix-run/react"
 import { Button } from "~/components/ui/button"
+import { Search, AlignJustify } from "lucide-react"
 
 const MenuBar = () => {
 	return (
-		<header className="container flex items-center justify-between  px-28 py-8">
-			<div className="flex items-center gap-4">
-				<img src="/img/logo.svg" alt="" />
-				<nav>
+		<header className="px-4 py-4">
+			<div className="flex items-center justify-between">
+				<div className="flex gap-4 items-center">
+					<AlignJustify className="sm:hidden text-gray-400" />
+					<div className="font-extrabold leading-tight text-xl bg-zinc-700 text-white p-2 rounded-md">seven</div>
+				</div>
+
+				<nav className="hidden sm:block">
 					<ul className="flex gap-4">
-						<li>Features</li>
-						<li>Princing</li>
-						<li>Resources</li>
+						<li>Come soon</li>
+						<li>Come soon</li>
+						<li>Come soon</li>
 					</ul>
 				</nav>
-			</div>
-			<div>
-				<span className="mr-4">Login</span>
-				<Button>Sign Up</Button>
+				<div className="flex gap-2 items-center">
+					<Search className="text-gray-400" />
+					<Button asChild variant="outline" size="lg">
+						<Link to="#">
+							Login
+						</Link>
+					</Button>
+				</div>
 			</div>
 		</header>
 	)
